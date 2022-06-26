7deef9dedae3645197563f820767833683b73459
Tiago Espanha <tiago.espanha@gmail.com>
2022-06-26 18:09:39 -0300

    Add knight move set

--- src/pieces/knight.py
+++ src/pieces/knight.py
@@ -1,18 +1,38 @@
+from turtle import back, pos
 from pieces.piece import Piece
+from constants import getForwardPosition, getSidewaysPosition
 
 class Knight(Piece): 
     
-    def move():
-        print('move')
-
-    def attack():
-        print('atk')
-
     def getMovementMoves(self):
-        pass
+        possiblePositions = []
+        posLabel = self.getBoardPosition()
+        
+        forwardPos = getForwardPosition(posLabel, 2, 1)
+        if len(forwardPos) == 2:            
+            possiblePositions.extend(getSidewaysPosition(forwardPos[1], 1, 1))
+            possiblePositions.extend(getSidewaysPosition(forwardPos[1], 1, -1))
+
+        backPos = getForwardPosition(posLabel, 2, -1)
+        if len(backPos) == 2:
+            possiblePositions.extend(getSidewaysPosition(backPos[1], 1, 1))
+            possiblePositions.extend(getSidewaysPosition(backPos[1], 1, -1))
+
+        leftPos = getSidewaysPosition(posLabel, 2, 1)
+        if len(leftPos) == 2:
+            possiblePositions.extend(getForwardPosition(leftPos[1], 1, 1))
+            possiblePositions.extend(getForwardPosition(leftPos[1], 1, -1))
+
+        rightPos = getSidewaysPosition(posLabel, 2, -1)
+        if len(rightPos) == 2:
+            possiblePositions.extend(getForwardPosition(rightPos[1], 1, 1))
+            possiblePositions.extend(getForwardPosition(rightPos[1], 1, -1))
+
+        return possiblePositions
+
     
     def getAttackMoves(self):
-        pass
+        return self.getMovementMoves()
         
     def posMove(self):
         pass
